70e515aabe13c83432530bec319d812f463e308e
Tom Fennelly <tom.fennelly@gmail.com>
2011-02-11 11:12:28 +0000

    get jndi working

--- bean/src/main/java/org/switchyard/component/bean/deploy/ApplicationServiceDescriptorSet.java
+++ bean/src/main/java/org/switchyard/component/bean/deploy/ApplicationServiceDescriptorSet.java
@@ -25,16 +25,20 @@ package org.switchyard.component.bean.deploy;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
  */
-public class ApplicationServiceDescriptorSet {
+public class ApplicationServiceDescriptorSet implements Serializable {
 
-    private static final String JAVA_COMP_SWITCHYARD_SERVICE_DESCRIPTOR_SET = "java:comp/switchyard/ApplicationServiceDescriptorSet";
+    private static final String JAVA_COMP_SWITCHYARD_SERVICE_DESCRIPTOR_SET = "cn=SwitchyardApplicationServiceDescriptorSet";
 
     private List<ServiceDescriptor> _descriptorSet = new ArrayList<ServiceDescriptor>();
 
@@ -47,34 +51,50 @@ public class ApplicationServiceDescriptorSet {
     }
     
     public static ApplicationServiceDescriptorSet bind() {
-        try {
-            Context jndiContext = new InitialContext();
+        Map<ClassLoader, ApplicationServiceDescriptorSet> descriptorMap = getApplicationDescriptorMap();
+        ApplicationServiceDescriptorSet appDescriptorSet = new ApplicationServiceDescriptorSet();
 
-            try {
-                ApplicationServiceDescriptorSet descriptorSet = new ApplicationServiceDescriptorSet();
-                jndiContext.bind(JAVA_COMP_SWITCHYARD_SERVICE_DESCRIPTOR_SET, descriptorSet);
-                return descriptorSet;
-            } finally {
-                jndiContext.close();
-            }
-        } catch (NamingException e) {
-            throw new IllegalStateException("Unexpected NamingException getting JNDI Context.", e);
-        }
+        descriptorMap.put(Thread.currentThread().getContextClassLoader(), appDescriptorSet);
+
+        return appDescriptorSet;
     }
 
     public static ApplicationServiceDescriptorSet lookup() {
+        return getApplicationDescriptorMap().get(Thread.currentThread().getContextClassLoader());
+    }
+
+    public static void unbind() {
+        getApplicationDescriptorMap().remove(Thread.currentThread().getContextClassLoader());
+    }
+
+    private synchronized static Map<ClassLoader, ApplicationServiceDescriptorSet> getApplicationDescriptorMap() {
         try {
             Context jndiContext = new InitialContext();
 
             try {
-                ApplicationServiceDescriptorSet descriptorSet = (ApplicationServiceDescriptorSet) 
-                    jndiContext.lookup(JAVA_COMP_SWITCHYARD_SERVICE_DESCRIPTOR_SET);
-                return descriptorSet;
+                Map<ClassLoader, ApplicationServiceDescriptorSet> descriptorMap = (Map<ClassLoader, ApplicationServiceDescriptorSet>)
+                            jndiContext.lookup(JAVA_COMP_SWITCHYARD_SERVICE_DESCRIPTOR_SET);
+
+                return descriptorMap;
             } finally {
                 jndiContext.close();
             }
-        } catch (NamingException e) {
-            throw new IllegalStateException("Unexpected NamingException getting JNDI Context.", e);
+        } catch (NamingException e1) {
+            try {
+                Context jndiContext = new InitialContext();
+
+                try {
+                    Map<ClassLoader, ApplicationServiceDescriptorSet> descriptorMap =
+                            new ConcurrentHashMap<ClassLoader, ApplicationServiceDescriptorSet>();
+                    jndiContext.bind(JAVA_COMP_SWITCHYARD_SERVICE_DESCRIPTOR_SET, descriptorMap);
+
+                    return descriptorMap;
+                } finally {
+                    jndiContext.close();
+                }
+            } catch (NamingException e2) {
+                throw new IllegalStateException("Unexpected NamingException getting JNDI Context.", e2);
+            }
         }
     }
 }
